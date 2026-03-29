c5f07d49e864008e7fe867a6e29f5de111f5a793
maksim15132 <maksim15132@mail.ru>
2026-03-29 12:48:08 +0300

    9.1
    9.1

--- ar-models/script.js
+++ ar-models/script.js
@@ -234,17 +234,16 @@
     const buttonsDiv = document.createElement("div"); buttonsDiv.className = "card-actions";
     
     const detailsBtn = document.createElement("button");  // создаём <a> вместо <button>
-    detailsBtn.className = "btn btn-primary";        // сохраняем те же стили
+    detailsBtn.className = "btn btn-primary big-btn";       // сохраняем те же стили
     detailsBtn.textContent = "Перейти в раздел";         // текст ссылки
     detailsBtn.href = "https://maksim15132.github.io/museum/ar-models/";                // адрес, куда ведёт ссылка
     detailsBtn.target = "_blank";                   // если хочешь открывать в новой вкладке
     
-    const addBtn = document.createElement("button"); addBtn.className = "btn btn-ghost"; addBtn.textContent = "В корзину";
-
+    
     detailsBtn.onclick = () => openModal(dish);
-    addBtn.onclick = () => { addToCart(dish, 1); alert("Добавлено в корзину"); };
+    
 
-    buttonsDiv.appendChild(detailsBtn); buttonsDiv.appendChild(addBtn);
+    buttonsDiv.appendChild(detailsBtn);
 
     content.appendChild(meta); content.appendChild(desc); content.appendChild(buttonsDiv);
 
